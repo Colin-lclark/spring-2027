99bd069cf13155db0eb12c2e970c76545629a2c3
Colin-lclark <lc25-0951@lclark.edu>
2026-03-06 11:42:26 -0800

    e

--- project/etl/etl.py
+++ project/etl/etl.py
@@ -1,1 +1,23 @@
-import Tree_Records
+import csv
+
+TREE_DATABASE_FILE_PATH = 'C:\Users\Colin\OneDrive\Documents\GitHub\Databases\spring-2026\project\etl'
+
+def createDatabaseCSV(database : str, tables : list[str]):
+    '''tables = [[tableName, [column1, column2, ...], [loc1, loc2, ...]], ...]'''
+    for tb in tables:
+        tableData()
+
+
+
+def tableData(columns : list[str], column_loc : list[int], database : str, file : str) -> list:
+    table = [columns]
+    id = 1
+    with open(database) as line:
+        table.append([id])
+        for i in column_loc:
+            table[len(table) - 1].append(list(csv.reader(line)[i]))
+
+    with open(file, mode='w', newline='') as row:
+        csv.writer(file).writerows(table)
+
+    return table
