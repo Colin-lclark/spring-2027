2a0f23e649e78f5fe604a5bcd9e606b29f063fe3
Colin-lclark <lc25-0951@lclark.edu>
2026-03-13 14:40:35 -0700

    sluuuuuuuuuuuuuuuurrrrrrrrrrrp

--- project/etl/etl.py
+++ project/etl/etl.py
@@ -1,26 +1,36 @@
 import csv
 
 TREE_DATABASE_TABLES = [
-    ['location', ['location_id', 'address', 'neighborhood'], [3, 4]],
-    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8]],
-    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15]],
+    ['location', ['location_id', 'address', 'neighborhood'], [3, 4, 2]],
+    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8, 2]],
+    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15, 2]],
     ['tree', ['tree_id', 'x_coord', 'y_coord', 'diameter', 'health', 'date_inventoried', 'location_id', 'species_id', 'site_id'], [0, 1, 9, 10, 5]]
   ]
 
+ALL_TABLES = []
+
 def createTableCSVs(tables : list[str]):
     '''tables = [[tableName, [column1, column2, ...], [loc1, loc2, ...]], ...]'''
-    database = getFilePath()
+    database = getFilePath('Database')
     for tb in tables:
-        writeCSV(getFilePath(), tableData(tb[2], database), tb[1])
+        ALL_TABLES.append(tableData(tb[2], database))
+    writeCSV(getFilePath(tb[0] + '.csv'), tableData(TREE_DATABASE_TABLES))
 
 
-def getFilePath() -> str:
-    print('Database File Path: ')
+def getFilePath(table : str) -> str:
+    print(table + 'File Path: ')
     file_path = input()
     return file_path
 
 
-def tableData(column_loc : list[int], database : str) -> csv:
+def getLocation(table : str):
+    for i in range(TREE_DATABASE_TABLES):
+        if TREE_DATABASE_TABLES[i][0] == table:
+            return TREE_DATABASE_TABLES[i][2]
+    return 0
+
+
+def tableData(columns : str, column_loc : list[int], database : str, name : str) -> csv:
     table = set()
     with open(database, mode='r') as row:
         value = csv.reader(row)
@@ -36,18 +46,30 @@ def tableData(column_loc : list[int], database : str) -> csv:
                         temp.append(val)
                 else:
                     temp.append(row[index])
+
+            #Get the species, site, and location ids for tree
+            if name == 'tree':
+                
+
             table.add(tuple(temp))
 
-    return table
+    return setToList(table, columns)
+
+def setToList(table : set, columns : list[str]):
+    id = 1
+    table_list = [columns]
+    for tup in table:
+        temp = list(tup)
+        temp.insert(0, id)
+        table_list.append(temp)
+        id += 1
+    return table_list
 
 
-def writeCSV(file : str, table : set, columns : list[str]):
-    id = 0
-    with open(file, mode='w') as row:
-        value = csv.writer(row)
-        for val in table:
-            value.writerows(list(val))
-            id += 1
+def writeCSV(file : str, table):
+
+    with open(file, mode='w', newline='') as row:
+        csv.writer(row).writerows(table)
 
 
 if __name__ == '__main__':
