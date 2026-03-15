44e2a1b8fb6a8ed6eb7b2daee2c0a27f4297c379
Colin-lclark <lc25-0951@lclark.edu>
2026-03-15 16:21:12 -0700

    Hopefully close

--- project/etl/etl.py
+++ project/etl/etl.py
@@ -1,20 +1,19 @@
 import csv
 
-TREE_DATABASE_TABLES = [
-    ['location', ['location_id', 'address', 'neighborhood'], [3, 4, 2]],
-    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8, 2]],
-    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15, 2]],
-    ['tree', ['tree_id', 'x_coord', 'y_coord', 'diameter', 'health', 'date_inventoried', 'location_id', 'species_id', 'site_id'], [0, 1, 9, 10, 5]]
-  ]
+AUTHORS = ('Fabrizio Martinez Valiente', 'Colin Sheehan')
 
-ALL_TABLES = []
+TREE_DATABASE_TABLES = [
+    ['location', ['location_id', 'address', 'neighborhood'], [3, 4]],
+    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8]],
+    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15]],
+    ['tree', ['tree_id', 'x_coord', 'y_coord', 'diameter', 'health', 'date_inventoried', 'location_id', 'species_id', 'site_id'], [2, 0, 1, 9, 10, 5]]
+  ]
 
 def createTableCSVs(tables : list[str]):
     '''tables = [[tableName, [column1, column2, ...], [loc1, loc2, ...]], ...]'''
     database = getFilePath('Database')
     for tb in tables:
-        ALL_TABLES.append(tableData(tb[2], database, tb[0]))
-    writeCSV(getFilePath(tb[0] + '.csv'), tableData(TREE_DATABASE_TABLES))
+        writeCSV(getFilePath(tb[0] + '.csv'), tableData(tb[1], tb[2], database, tb[0]))
 
 
 def getFilePath(table : str) -> str:
@@ -23,15 +22,11 @@ def getFilePath(table : str) -> str:
     return file_path
 
 
-def getLocation(table : str):
-    for i in range(TREE_DATABASE_TABLES):
-        if TREE_DATABASE_TABLES[i][0] == table:
-            return TREE_DATABASE_TABLES[i][2]
-    return 0
+def tableData(columns : list[str], column_loc : list[int], database : str, name : str) -> list[tuple]:
+    table = [columns]
+    table_set = set()
+    id = 0
 
-
-def tableData(column_loc : list[int], database : str, name : str) -> csv:
-    table = set()
     with open(database, mode='r') as row:
         value = csv.reader(row)
         for row in value:
@@ -47,33 +42,21 @@ def tableData(column_loc : list[int], database : str, name : str) -> csv:
                 else:
                     temp.append(row[index])
 
-            if name == 'tree':
-                for i in ALL_TABLES:
-                    for j in i:
-                        if j[len(j) - 1] == row[2]:
-                            temp.append(j[0])
-                            break
-            #Get the species, site, and location ids for tree
-            table.add(tuple(temp))
+            length = len(table_set)
+            table_set.add(tuple(temp))
+
+            if length != len(table_set):
+                id += 1
+                temp.insert(0, id)
+                table.append(temp)
 
     return table
 
-def setToList(table : set, columns : list[str]):
-    id = 1
-    table_list = [columns]
-    for tup in table:
-        temp = list(tup)
-        temp.insert(0, id)
-        table_list.append(temp)
-        id += 1
-    return table_list
 
+def writeCSV(file : str, table : list[tuple]):
 
-def writeCSV(file : str, table : set, columns : list[str]):
-
-    table_list = setToList(table, columns)
     with open(file, mode='w', newline='') as row:
-        csv.writer(row).writerows(table_list)
+        csv.writer(row).writerows(table)
 
 
 if __name__ == '__main__':
