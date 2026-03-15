755a9a5292f262975db10d3206294fd6815eeffe
Colin-lclark <lc25-0951@lclark.edu>
2026-03-14 23:42:19 -0700

    Bad version

--- project/etl/etl.py
+++ project/etl/etl.py
@@ -1,9 +1,9 @@
 import csv
 
 TREE_DATABASE_TABLES = [
-    ['location', ['location_id', 'address', 'neighborhood'], [3, 4, 2]],
-    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8, 2]],
-    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15, 2]],
+    ['location', ['location_id', 'address', 'neighborhood'], [3, 4]],
+    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8]],
+    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15]],
     ['tree', ['tree_id', 'x_coord', 'y_coord', 'diameter', 'health', 'date_inventoried', 'location_id', 'species_id', 'site_id'], [0, 1, 9, 10, 5]]
   ]
 
@@ -13,12 +13,12 @@ def createTableCSVs(tables : list[str]):
     '''tables = [[tableName, [column1, column2, ...], [loc1, loc2, ...]], ...]'''
     database = getFilePath('Database')
     for tb in tables:
-        ALL_TABLES.append(tableData(tb[2], database))
-    writeCSV(getFilePath(tb[0] + '.csv'), tableData(TREE_DATABASE_TABLES))
+        ALL_TABLES.append(tableData(tb[1],tb[2], database, tb[0]))
+    writeCSV(getFilePath(tb[0] + '.csv'), TREE_DATABASE_TABLES[3][1])
 
 
 def getFilePath(table : str) -> str:
-    print(table + 'File Path: ')
+    print(table + ' File Path: ')
     file_path = input()
     return file_path
 
@@ -48,12 +48,10 @@ def tableData(columns : str, column_loc : list[int], database : str, name : str)
                     temp.append(row[index])
 
             #Get the species, site, and location ids for tree
-            if name == 'tree':
-                
-
             table.add(tuple(temp))
 
     return setToList(table, columns)
+
 
 def setToList(table : set, columns : list[str]):
     id = 1
