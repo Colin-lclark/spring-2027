f7efc5c4a0b1b9d20ab35d412635edb608edf101
Colin-lclark <lc25-0951@lclark.edu>
2026-03-15 22:03:51 -0700

    death

--- project/etl/etl.py
+++ project/etl/etl.py
@@ -3,17 +3,23 @@ import csv
 AUTHORS = ('Fabrizio Martinez Valiente', 'Colin Sheehan')
 
 TREE_DATABASE_TABLES = [
-    ['location', ['location_id', 'address', 'neighborhood'], [3, 4]],
-    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8]],
-    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15]],
-    ['tree', ['tree_id', 'x_coord', 'y_coord', 'diameter', 'health', 'date_inventoried', 'location_id', 'species_id', 'site_id'], [2, 0, 1, 9, 10, 5]]
-  ]
+    ['tree', ['tree_id', 'x_coord', 'y_coord', 'diameter', 'health', 'date_inventoried', 'location_id', 'species_id', 'site_id'], [0, 1, 9, 10, 5]],
+    ['location', ['location_id', 'address', 'neighborhood'], [3, 4, 2]],
+    ['species', ['species_id', 'taxonomy', 'common_name', 'functional_type', 'mature_size'], [6, 7, 8, 2]],
+    ['site', ['site_id', 'site_type', 'site_size', 'site_width', 'wires', 'improvement'], [11, 12, 13, 14, 15, 2]],
+]
 
 def createTableCSVs(tables : list[str]):
     '''tables = [[tableName, [column1, column2, ...], [loc1, loc2, ...]], ...]'''
     database = getFilePath('Database')
+    ids = []
+    for n in range(252206):
+        ids.append([])
+    i = 0
     for tb in tables:
-        writeCSV(getFilePath(tb[0] + '.csv'), tableData(tb[1], tb[2], database, tb[0]))
+        new = tableData(tb[2], database, tb[0])
+        formatCSVs(new, tb[0])
+        writeCSV(getFilePath(tb[0] + '.csv'), new)
 
 
 def getFilePath(table : str) -> str:
@@ -22,16 +28,15 @@ def getFilePath(table : str) -> str:
     return file_path
 
 
-def tableData(columns : list[str], column_loc : list[int], database : str, name : str) -> list[tuple]:
-    table = [columns]
-    table_set = set()
-    id = 0
+def tableData(column_loc : list[int], database : str, name : str) -> list[tuple]:
+    table = []
+    n = 0
 
     with open(database, mode='r') as row:
         value = csv.reader(row)
         for row in value:
             temp = []
-
+            
             #Adds all values specified for the given table
             for index in column_loc:
 
@@ -41,15 +46,29 @@ def tableData(columns : list[str], column_loc : list[int], database : str, name 
                         temp.append(val)
                 else:
                     temp.append(row[index])
+            if n != 0:
+                table.append(temp)
+            if n == 0:
+                n += 1
 
+    return table
+
+def formatCSVs(table : list[list[str]], ids : list[list[int]], name : str):
+    table_set = set()
+    id = 0
+
+    if name != 'tree':
+        for i in table:
             length = len(table_set)
+            temp = i[:len(i) - 1]
             table_set.add(tuple(temp))
 
             if length != len(table_set):
                 id += 1
                 temp.insert(0, id)
-                table.append(temp)
-
+            
+            ids[int(i[len(i) - 1])].append()
+    
     return table
 
 
